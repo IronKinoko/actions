c163eb17955257e22b63cdaabdf98d362d7621c0
IronKinoko <kinoko_main@outlook.com>
2021-02-20 21:55:37 +0800

    feat: add signfl

--- scripts/signFL.js
+++ scripts/signFL.js
@@ -1,1 +1,23 @@
-console.log(process.env.FLCOOKIES)
+const axios = require('axios').default
+const cookies = process.env.FLCOOKIES
+const url = 'https://fastlink.ws/user'
+
+async function signFL() {
+  const res = await axios.post(
+    url,
+    {},
+    {
+      headers: {
+        Cookies: cookies,
+        'user-agent':
+          'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36',
+      },
+    }
+  )
+
+  const msg = res.data.msg
+
+  console.log(msg)
+}
+
+signFL()
